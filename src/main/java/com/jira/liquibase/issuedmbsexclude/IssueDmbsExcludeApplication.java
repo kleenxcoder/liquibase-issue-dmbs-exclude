dafa2cc77b4e66c4ae34778f9ac1789750e04184
Stritzinger Peter <peter.stritzinger@abf.at>
2022-09-09 13:46:32 +0200

    add spring project

---
+++ src/main/java/com/jira/liquibase/issuedmbsexclude/IssueDmbsExcludeApplication.java
@@ -0,0 +1,13 @@
+package com.jira.liquibase.issuedmbsexclude;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssueDmbsExcludeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssueDmbsExcludeApplication.class, args);
+	}
+
+}
